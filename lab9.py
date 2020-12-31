ce7dd11de06e7b32093289562d79a0f64082e2e3
EC2 Default User <ec2-user@ip-172-31-9-62.ec2.internal>
2020-12-31 21:14:51 +0000

    lab9

--- lab9.py
+++ lab9.py
@@ -1,48 +1,45 @@
-import lec9 
+'''
+lab 9  class lab
+'''
 
-my_car = lec9.car('corolla')
-
-print(my_car.report())
-
-import numpy 
-
-print(numpy)
-
-class math_cal():
-
+#3.1
+class my_stat():
+    
     def cal_sigma(self,m,n):
-        
+    
         result = 0
         
         for i in range(n,m+1):
             result = result +i
         return result
-
-    
-    def cal_pi(self,m,n):
         
+    def cal_pi(self,m,n):
+    
         result = 1
         
         for i in range(n,m+1):
             result = result*i
         return result
         
-
-    
     def cal_f(self,m):
-        
+    
         if m ==0:
             return 1
         else:
             return m * self.cal_f(m-1)
-
-    
+            
     def cal_p(self,m,n):
+        
         return self.cal_f(m)/self.cal_f(m-n)
 
-my_cal = math_cal()
+#3.2
+
+my_cal = my_stat()
+
+print(my_cal.cal_sigma(5,3))
+
+print(my_cal.cal_pi(5,3))
 
 print(my_cal.cal_f(5))
-print(my_cal.cal_pi(5,3))
-print(my_cal.cal_sigma(5,3))
+
 print(my_cal.cal_p(5,2))
